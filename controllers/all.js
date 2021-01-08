e7d29235667ba81ef68c88ae1970b746f6c87a41
snakehead007 <snakehead007@pm.me>
2021-01-08 21:03:59 +0100

    update to v0.1.1

--- controllers/all.js
+++ controllers/all.js
@@ -2,66 +2,54 @@ const {increaseApiCallAmount,hasUserExceededAPiAmount} = require('fyrebrick-help
 const {User} = require('fyrebrick-helper').models;
 const {bricklink} = require("fyrebrick-helper").helpers;
 const {logger} = require('fyrebrick-helper').helpers;
+const superagent = require('superagent');
 const TIMEOUT_RESTART = 20*100;
 
 module.exports = async (req,res,next)=>{
     res.setHeader('content-type', 'application/json');
-    id = req.body._id;
-    const user = await User.findOne({_id:id,setUpComplete:true},(err)=>{
-        if(err){
-            logger.error(`Finding user by id ${id} gave an error ${err}`);
-        }
-    });
-    if(!user){
-        logger.error(`No user was found by id ${id}`);
-        res.send({success:false});
-    }else{
-        try{
-            if(await hasUserExceededAPiAmount(id)){
-                res.send({meta:{
-                    code:429,
-                    message:'User has exceeded the API limit of bricklink'
-                }});
-                return;
-            }
-            increaseApiCallAmount(id);
-            let s1 = await bricklink.order.all(user);
-            if(s1===false){
-                logger.warn(`ordersAll was not successful for user ${user.email}, retrying in 20sec...`);
-                s1 = timeout(await bricklink.order.all,TIMEOUT_RESTART,user);
-                if(s1===false){      
-                    res.send({success: false});
-                    return;
-                }else{
-                    res.send({success: true});
-                    return;
-                }
-            }
-            if(await hasUserExceededAPiAmount(id)){
-                res.send({meta:{
-                    code:429,
-                    message:'User has exceeded the API limit of bricklink'
-                }});
-                return;
-            }
-            increaseApiCallAmount(id);
-            let s2 = await bricklink.inventory.all(user);
-            if(s2===false){
-                logger.warn(`inventoryAll was not successful for user ${user.email}, retrying in 20sec...`);
-                s2 = timeout(await bricklink.inventory.all,TIMEOUT_RESTART,user);
-                if(s2===false){
-                    res.send({success: false});
-                    return;
-                }else{
-                    res.send({success: true});
-                    return;
-                }
-            }
-        }catch(err){
-            res.send({success: false});
-            return;
-        }
-        res.send({success: true});
-        
-    }
+    
+    console.log('jup');
+    const status = await doAll(req);
+    res.send({success: true});
 };
+
+const doAll = (req) => {
+    let s1,s2 = false;
+    return new Promise(async (resolve,reject)=>{
+        id = req.body._id;
+        const user = await User.findOne({_id:id,setUpComplete:true},(err)=>{
+            if(err){
+                logger.error(`Finding user by id ${id} gave an error ${err}`);
+            }
+        });
+        if(!user){
+            logger.error(`No user was found by id ${id}`);
+            res.send({success:false});
+        }else{
+            try{
+                if(await hasUserExceededAPiAmount(id)){
+                    res.send({meta:{
+                        code:429,
+                        message:'User has exceeded the API limit of bricklink'
+                    }});
+                    return;
+                }
+                increaseApiCallAmount(id);
+                await bricklink.order.all(user);
+                if(await hasUserExceededAPiAmount(id)){
+                    res.send({meta:{
+                        code:429,
+                        message:'User has exceeded the API limit of bricklink'
+                    }});
+                    return;
+                }
+                increaseApiCallAmount(id);
+                await bricklink.inventory.all(user);
+            }catch(err){
+                reject(err);
+                return;
+            }
+            resolve(user);
+        }
+    })
+}
