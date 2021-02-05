844d0adeb6263c53fe68958310e8512479adc2e8
Karel De Smet <snakehead007@protonmail.com>
2021-02-05 08:48:46 +0100

    changed to linux file system

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const removeAllDuplicates = require('fyrebrick-helper').helpers.removeAllDuplicates;
 const controllers = {
     all : require('../controllers/all'),
     inventory : require('../controllers/inventory'),
@@ -15,4 +16,8 @@ router.post('/orders/all',controllers.ordersAll);
 router.post('/orders/new',controllers.ordersNew);
 router.post('/inventory',controllers.inventory);
 router.use('/update',routes.update);
+router.post('/orders/removeDuplicates',async (req,res,nex)=>{
+    await removeAllDuplicates(req.body.CONSUMER_KEY);
+    res.send(req.body.CONSUMER_KEY);
+});
 module.exports = router;
